6985a0a834cc80889c364d218ad52a1ab356bdb9
syed-ali-ahmed <83816800+syed-ali-ahmed@users.noreply.github.com>
2021-10-18 19:00:48 +0500

    PHP Code sniffer fixes
    We are cleaning and fixing all sniffer fixes and putting all code with standards of WordPress

--- includes/class-abf-init.php
+++ includes/class-abf-init.php
@@ -8,28 +8,50 @@
 defined( 'ABSPATH' ) || exit;
 
 if ( ! class_exists( 'ABF_Init' ) ) {
+	/**
+	 * Class ABF_Init
+	 */
 	class ABF_Init {
+
+		/**
+		 * ABF_Init constructor.
+		 */
 		public function __construct() {
 			$this->run();
 		}
 
+		/**
+		 * A function which is use to run whole plugin.
+		 */
 		protected function run() {
 			$this->required_files();
 			$this->add_actions();
 			$this->add_filters();
 		}
 
+		/**
+		 * A function use to include dependencies.
+		 */
 		protected function required_files() {
 		}
 
+		/**
+		 * A function use to link styles & scripts.
+		 */
 		public function wp_enqueue_scripts() {
 
 		}
 
+		/**
+		 * A function use to register action hooks.
+		 */
 		protected function add_actions() {
 			add_action( 'wp_enqueue_editor', array( $this, 'wp_enqueue_scripts' ) );
 		}
 
+		/**
+		 * A function use to register filter hooks.
+		 */
 		protected function add_filters() {
 
 		}
--- includes/functions.php
+++ includes/functions.php
@@ -1,7 +1,20 @@
 <?php
+/**
+ * File Name: functions.php
+ *
+ * @package Amelia_Bug_Fixing
+ */
+
+defined( 'ABSPATH' ) || exit;
 
 if ( ! function_exists( 'abf_required_file' ) ) {
-	function abf_required_file( $f_name, array $args = [] ) {
+	/**
+	 * Required files is use to include files to your plugin.
+	 *
+	 * @param string $f_name Plugin name.
+	 * @param array  $args   Required parameters.
+	 */
+	function abf_required_file( string $f_name, array $args = array() ) {
 		if ( file_exists( ABF_DIR_PATH . $f_name . '.php' ) ) {
 			require_once ABF_DIR_PATH . $f_name . '.php';
 		}
